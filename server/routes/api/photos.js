384715f5339953e38d5d08ac2d717b47c20dc9eb
Shehryar Assad <xshehryar@gmail.com>
2019-10-12 18:25:27 -0400

    Create photo deletion endpoint

--- server/routes/api/photos.js
+++ server/routes/api/photos.js
@@ -61,17 +61,71 @@ router.post(
 );
 
 /**
+ * @route   DELETE api/photos/delete
+ * @desc    Deletes list of image IDs associated to a user account
+ * @access  Private
+ */
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // photo ids from body
+    const { photoIds } = req.body;
+
+    let succeededIds = [];
+    let failedIds = [];
+
+    // return type
+    class PhotoResponse {
+      constructor(id, succeeded, message) {
+        this.id = id;
+        this.message =  message || (succeeded ? "Deletion succeeded" : "Deletion failed")
+      }
+
+      toString() {
+        return {id, message }
+      }
+    }
+
+    // create array of promises
+    const promises = photoIds.map(async photoId => {
+      return new Promise(async (resolve, _reject) => {
+        try {
+          // find photo and delete
+          const photo = await Photo.find({ id: photoId, user: req.user.id })
+          if (photo) {
+            await photo.delete();
+            succeededIds.push(new PhotoResponse(photoId, true));
+          } else {
+            failedIds.push(new PhotoResponse(photoId, false, "Photo not found"));
+          }
+        } catch (e) {
+          console.error(e);
+          failedIds.push(new PhotoResponse(photoId, false));
+        }
+        resolve();
+      })
+    });
+
+    // wait until all photos are deleted
+    await Promise.all(promises);
+
+    // return deletion status
+    return res.json({ succeededIds, failedIds});
+  }
+);
+
+/**
  * @route   POST api/photos/all
  * @desc    Returns list of all image URLs associated to a user account
  * @access  Private
  */
-router.post(
-  "/all",
+router.get(
+  "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    // TODO: Is this how to retrieve multiple images?
     // fetch all photos, ordered by date
-    Photo.find({ user: req.body.user.id })
+    Photo.find({ user: req.user.id })
       .sort({ date: -1 })
       .then(photos => res.json(photos))
       .catch(err => res.status(404).message("Unable to retrieve photos"));
