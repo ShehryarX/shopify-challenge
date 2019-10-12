22ce090feaea203ac3126964bbe0a6a4ad132e4a
Shehryar Assad <xshehryar@gmail.com>
2019-10-12 15:32:46 -0400

    Create register user route

--- server/routes/api/users/index.js
+++ server/routes/api/users/index.js
@@ -65,16 +65,19 @@ router.post("/register", async (req, res) => {
  */
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
+  const errors = {};
 
-  // find user by email
-  User.findOne({ email }).then(user => {
+  try {
+    const user = User.findOne({ email });
+
+    // user isn't found
     if (!user) {
-      // user not found
       errors.email = "User not found";
       return res.status(404).json(errors);
     }
 
     bcrypt.compare(password, user.password).then(isMatched => {
+      // incorrect password
       if (!isMatched) {
         errors.password = "Password incorrect";
         return res.status(400).json(errors);
@@ -84,15 +87,22 @@ router.post("/login", (req, res) => {
       const { id, name, avatar } = user;
       const payload = { id, name, avatar }; // create JWT payload
 
-      // sign token
-      jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-        res.json({
-          success: true,
-          token: "Bearer " + token
-        });
-      });
+      // sign and return token, expires in 24 hours
+      jwt.sign(
+        payload,
+        keys.secretOrKey,
+        { expiresIn: "24h" },
+        (err, token) => {
+          return res.json({
+            success: true,
+            token: `Bearer ${token}`
+          });
+        }
+      );
     });
-  });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 module.exports = router;
