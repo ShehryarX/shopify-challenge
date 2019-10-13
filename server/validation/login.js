a66393997fd4febd8777457d805339c4e2ac2fbb
Shehryar Assad <xshehryar@gmail.com>
2019-10-12 21:27:41 -0400

    Create login validation

---
+++ server/validation/login.js
@@ -0,0 +1,22 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  const { email, password } = data;
+
+  if (!Validator.isEmail(email)) errors.email = "Email is invalid";
+  if (Validator.isEmpty(email)) errors.email = "Email field is required";
+
+  if (Validator.isEmpty(password))
+    errors.password = "Password field is required";
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
